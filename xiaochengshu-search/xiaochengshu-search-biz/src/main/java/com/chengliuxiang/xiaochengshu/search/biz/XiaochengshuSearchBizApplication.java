d4bcd9727ad230abc829b49f626f1335678fd0bd
Shawn <ScoMorii@163.com>
2026-02-14 14:16:12 +0800

    搜索服务搭建

---
+++ xiaochengshu-search/xiaochengshu-search-biz/src/main/java/com/chengliuxiang/xiaochengshu/search/biz/XiaochengshuSearchBizApplication.java
@@ -0,0 +1,11 @@
+package com.chengliuxiang.xiaochengshu.search.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaochengshuSearchBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaochengshuSearchBizApplication.class, args);
+    }
+}
