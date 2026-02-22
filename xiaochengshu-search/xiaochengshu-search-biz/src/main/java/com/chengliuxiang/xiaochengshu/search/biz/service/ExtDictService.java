330ae80ec10e646ba767bddf1ae276a89cc89398
Shawn <ScoMorii@163.com>
2026-02-22 11:12:33 +0800

    搜索服务——ik自定义热重载词典接口开发

---
+++ xiaochengshu-search/xiaochengshu-search-biz/src/main/java/com/chengliuxiang/xiaochengshu/search/biz/service/ExtDictService.java
@@ -0,0 +1,12 @@
+package com.chengliuxiang.xiaochengshu.search.biz.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface ExtDictService {
+
+    /**
+     * 获取热更新词典
+     * @return
+     */
+    ResponseEntity<String> getHotUpdateExtDict();
+}
